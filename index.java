0b868dd0bf9823d7136379216870ca7774d2b124
Rollo-the-coder <erikgotta@gmail.com>
2021-01-21 09:47:09 +0700

    basic addition and variables added

--- index.java
+++ index.java
@@ -3,5 +3,13 @@ public class index
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.print("I will learn Java!");
+        System.out.println(" Space testing");
+
+        int workHours = 9;
+        int homeHours = 15;
+        int totalHours = workHours + homeHours;
+        System.out.print("I have " + totalHours + " hours in a day");
+
     }
 }
